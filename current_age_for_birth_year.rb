1aa4a550d1eaa8b9aaa5df502551e9f8f14982d1
Kevin Nwoke <kevinnwoke@yahoo.com>
2018-08-15 04:26:20 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,7 +1,7 @@
 def current_age_for_birth_year(year)
-  current_year = 2018
+  current_year = 20004
   age_of_person = current_year - year
   return age_of_person
 end
 
-current_age_for_birth_year(1999)
+
